4deee2f0806d37b907457fc3c30811f392e7b17a
adrianzenuni <az40614@ubt-uni.net>
2020-01-13 00:20:48 +0100

    Updates

---
+++ precache-manifest.9439a70cc439c8566176554f18c819ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2140247e59178e15599d15bf4e8167d2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "02eb118aba6bce8c79d5",
+    "url": "/monsters-rolodex/static/css/main.1cef7808.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02eb118aba6bce8c79d5",
+    "url": "/monsters-rolodex/static/js/main.5af67de5.chunk.js"
+  },
+  {
+    "revision": "fcc6c7087f08bfeac99a",
+    "url": "/monsters-rolodex/static/js/runtime-main.52149680.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
